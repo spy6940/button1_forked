58d7b3c57a64d5d84d22df763f74b1cda964888a
seyoung <bsy6940@naver.com>
2023-09-11 11:45:52 +0900

    commit change

--- button1.py
+++ button1.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Yeon Ryu")
-b2 = Button(window, text="60207777")
+b1 = Button(window, text="Se Young")
+b2 = Button(window, text="60222092")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
